2cf1d0b20f27331018b097da36b4e24e14bce92d
bir-dv <brukjoseph@gmail.com>
2025-03-28 14:06:59 +0300

    Configure for GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  ssr: false,
   app: {
+    baseURL: '/udemy-course',
     head: {
       link: [
         {
